276183f51a4f98950abd1ab0cb3e02fabf651b03
Robbert Ottenbros <robbert.ottenbros@edu-kw1c.nl>
2020-02-21 12:39:46 +0100

    Huiswerkopdracht 2.1

---
+++ Hoofdstuk 2/opdracht2.1.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * User: Robbert
+ * Date: 7-2-2020
+ * Time: 14:05
+ * File: Opdracht_2.1.php
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+    Opdracht 2.1
+    </title>
+
+    <meta name="description" content="PHP">
+    <meta name="keywords" content="PHP">
+    <meta charset="utf-8">
+    <link href="../css/style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<h2>Uitwerking van <a href="../index.php"> PHP-opdrachten</a></h2>
+<div class="box">
+    <?php
+    echo ("<p>Hello World!</p>")
+    ?>
+</div>
+</body>
+</html>
