b6820b1387057d06ad2e5e900c5948d986d4053b
Robbert Ottenbros <robbert.ottenbros@edu-kw1c.nl>
2020-03-06 12:20:08 +0100

    Opdracht 3.3 verbetering

--- Hoofdstuk 3/opdracht3.3.php
+++ Hoofdstuk 3/opdracht3.3.php
@@ -1,25 +1,30 @@
+
 <?php
 /**
- * User: Robbert
- * Date: 2-3-2020
- * Time: 12:45
- * File: opdracht_3.3.php
+ * User: robbert
+ * Date: 7-2-2020
+ * Time: 13:55
+ * File: opdracht_2.1.php
  */
-include "../includes/variabelen.php";
-include "../includes/header.php";
 ?>
 
-    <h3>Taak 1</h3>
-    <div class="well well-lg">
-        Uitwerking opdracht
-    </div>
-    <h3>Taak 2</h3>
-    <div class="well well-lg">
-        Uitwerking opdracht
-    </div>
-    <h2>Uitwerking van <a href="../index.php"> PHP-opdrachten</a></h2>
-    <div class="box">
-    </div>
-    <?php
-    include "../includes/footer.php";
-    ?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+
+    </title>
+
+    <meta name="description" content=" ">
+    <meta name="keywords" content=" ">
+    <meta charset="utf-8">
+    <link href="../css/style.css" type="text/css" rel="stylesheet">
+
+</head>
+<body>
+<h2>Uitwerking van <a href="../index.php"> PHP-opdrachten</a></h2>
+<div class="box">
+    <h1>Taak 1</h1>
+</div>
+</body>
+</html>
